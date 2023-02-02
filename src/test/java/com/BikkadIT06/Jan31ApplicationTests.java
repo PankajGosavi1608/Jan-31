f8eb5c0fcaadc71b030642d4c04388fff715de49
PankajGosavi1608 <pankajudaygosawi@gmail.com>
2023-02-02 18:44:12 +0530

    Project Upload

---
+++ src/test/java/com/BikkadIT06/Jan31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jan31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
